bd8b2c0d17cdb3bae2ef8c3d9eb51e71ed1c814f
arcnmx <git@git.arcn.mx>
2025-06-09 21:48:16 -0700

    chore(space): add context to error messages

--- src/space/engine.rs
+++ src/space/engine.rs
@@ -14,7 +14,7 @@ use {
         timer::{PhaseState, RotationType, TimerFile, TimerMarker},
         ADDON_DIR,
     },
-    anyhow::anyhow,
+    anyhow::{anyhow, Context},
     bevy_ecs::prelude::*,
     glam::{Mat4, Vec3, Vec3Swizzles},
     itertools::Itertools,
@@ -92,12 +92,15 @@ impl Engine {
     pub fn initialise(ui: &Ui, receiver: Receiver<SpaceEvent>) -> anyhow::Result<Engine> {
         let addon_dir = &*ADDON_DIR;
 
-        let render_backend = RenderBackend::setup(&addon_dir, ui.io().display_size)?;
+        let render_backend = RenderBackend::setup(&addon_dir, ui.io().display_size)
+            .context("Failed to set up render backend")?;
 
         let models_dir = addon_dir.join("models");
-        let object_descs = ObjectLoader::load_desc(&models_dir)?;
+        let object_descs = ObjectLoader::load_desc(&models_dir)
+            .context("Failed to load model descriptors")?;
         log::debug!("{:?}", object_descs);
-        let model_files = ObjFile::load(&models_dir, &object_descs)?;
+        let model_files = ObjFile::load(&models_dir, &object_descs)
+            .context("Failed to load model object")?;
 
         let object_kinds = object_descs.to_backings(
             &render_backend.device,
@@ -112,8 +115,10 @@ impl Engine {
 
         schedule.add_systems(handle_marker_timings);
 
-        let mut packs = PackCollection::new(&render_backend)?;
-        packs.load_all(&addon_dir.join("pathing"))?;
+        let mut packs = PackCollection::new(&render_backend)
+            .context("pack building failed")?;
+        packs.load_all(&addon_dir.join("pathing"))
+            .context("pathing pack loading failed")?;
 
         let mut engine = Engine {
             model_files,
@@ -153,7 +158,7 @@ impl Engine {
                     &self.render_backend,
                     marker,
                     base_path.clone(),
-                )?);
+                ).context("marker object creation failed")?);
                 let entity = self.world.spawn((
                     Position(marker.position),
                     Marker {
@@ -206,8 +211,10 @@ impl Engine {
             Ok(event) => {
                 use SpaceEvent::*;
                 match event {
-                    MarkerFeed(phase_state) => self.new_phase(phase_state)?,
-                    MarkerReset(timer) => self.remove_phase(timer)?,
+                    MarkerFeed(phase_state) => self.new_phase(phase_state)
+                        .context("marker new phase")?,
+                    MarkerReset(timer) => self.remove_phase(timer)
+                        .context("marker remove phase")?,
                 }
             }
             Err(_error) => (),
@@ -222,7 +229,8 @@ impl Engine {
 
     pub fn render(&mut self, ui: &Ui) -> anyhow::Result<()> {
         let display_size = ui.io().display_size;
-        self.process_event()?;
+        self.process_event()
+            .context("render engine event processing failure")?;
         self.schedule.run(&mut self.world);
         let backend = &mut self.render_backend;
         backend.prepare(&display_size);
