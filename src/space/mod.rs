174b39e724734f31c17291a15d6b7e9941fd16b2
arcnmx <git@git.arcn.mx>
2025-06-10 22:13:27 -0700

    chore(space): const MAX_DEPTH

--- src/space/mod.rs
+++ src/space/mod.rs
@@ -6,3 +6,13 @@ pub mod render_list;
 pub mod resources;
 
 pub use engine::Engine;
+
+pub const MAX_DEPTH: f32 = 1000.0;
+pub const MIN_DEPTH: f32 = 0.1;
+
+pub const fn max_depth() -> f32 {
+    MAX_DEPTH
+}
+pub const fn min_depth() -> f32 {
+    MIN_DEPTH
+}
