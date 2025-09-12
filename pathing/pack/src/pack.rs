233831df03878b3580f66786d71952259741a44e
arcnmx <git@git.arcn.mx>
2025-09-12 00:40:52 -0700

    fix(pack): more cleanup and support

--- pathing/pack/src/pack.rs
+++ pathing/pack/src/pack.rs
@@ -95,6 +95,10 @@ pub fn parse_pack_def(
             .replace("R&D Waypoint", "R&amp;D Waypoint")
             .replace(" & ", " &amp; ")
             .replace("Remains&DESTROY", "Remains&amp;DESTROY")
+            // reactif-en
+            // TODO: resetoffset? hascountdown?
+            .replace("resetlenght=", "resetlength=")
+            .replace("nimsize=", "minsize=")
         ,
         #[allow(unreachable_patterns)]
         xml => xml,
@@ -204,14 +208,14 @@ fn inner_parse_pack_def(
                     "poi" => match Poi::from_xml(asset, attributes) {
                         Ok(poi) => parse_stack.push(PartialItem::Poi(poi)),
                         Err(e) => {
-                            log::warn!("POI parse failed: {e:?}");
+                            log::warn!("POI parse failed in {asset}: {e:?}");
                             parse_stack.push(PartialItem::PoisonElem);
                         }
                     },
                     "trail" => match Trail::from_xml(ctx, asset, attributes) {
                         Ok(trail) => parse_stack.push(PartialItem::Trail(trail)),
                         Err(e) => {
-                            log::warn!("Trail parse failed: {e:?}");
+                            log::warn!("Trail parse failed in {asset}: {e:?}");
                             parse_stack.push(PartialItem::PoisonElem);
                         }
                     },
--- pathing/pack/src/trail/mod.rs
+++ pathing/pack/src/trail/mod.rs
@@ -31,6 +31,7 @@ impl Trail {
         let mut category = String::new();
         let mut trail_path = None;
         let mut guid = None;
+        let mut map_id = None::<i32>;
         let mut attributes = MarkerAttributes::default();
 
         for attr in attrs {
@@ -40,6 +41,13 @@ impl Trail {
                 trail_path = Some(attr.value);
             } else if attr.name.local_name.eq_ignore_ascii_case("guid") {
                 guid = Some(taco_xml_to_guid(&attr.value));
+            } else if attr.name.local_name.eq_ignore_ascii_case("mapid") {
+                match attr.value.parse() {
+                    Ok(id) => map_id = Some(id),
+                    Err(e) => {
+                        log::warn!("failed to parse trail MapID {:?}: {e}", attr.value)
+                    },
+                }
             } else if let Err(..) = attributes.try_add(attr.name.borrow(), attr.value) {
                 log::warn!("Unknown Trail attribute '{}'", attr.name);
             }
@@ -54,6 +62,11 @@ impl Trail {
         };
 
         let data = read_trl_file(BufReader::new(ctx.find_asset_near(asset, &trail_path)?), &trail_path)?;
+        if let Some(map_id) = map_id {
+            if map_id != data.map_id {
+                log::warn!("trail MapID mismatch on {trail_path}: {map_id} vs {} from trl", data.map_id);
+            }
+        }
         let guid = guid.unwrap_or_default();
 
         let parent_path = Path::new(asset).parent()
