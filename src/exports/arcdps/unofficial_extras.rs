3abd24aeb0af8bc941121d9a4f23452b52430580
arcnmx <git@git.arcn.mx>
2025-09-17 05:30:27 -0700

    fix(arc): ooh a magic box, neat

--- src/exports/arcdps/unofficial_extras.rs
+++ src/exports/arcdps/unofficial_extras.rs
@@ -143,7 +143,7 @@ mod hotload {
 
     pub(crate) static CALLBACKS: Mutex<ExtrasCallbacks> = Mutex::new(ExtrasCallbacks::EMPTY);
     pub(crate) static JIT: LazyLock<Mutex<JitAllocator>> = LazyLock::new(|| {
-        // TODO: impl JitAlloc for box etc
+        // TODO: impl JitAlloc for a newtype around this
         Mutex::new(new_jit())
     });
     type StaticJitAlloc = &'static LazyLock<Mutex<JitAllocator>>;
@@ -156,11 +156,7 @@ mod hotload {
             immediate_release: true,
             .. Default::default()
         };
-        let jit = mem::ManuallyDrop::new(JitAllocator::new(opts));
-        let jit = unsafe {
-            std::ptr::read(&**jit)
-        };
-        jit
+        *JitAllocator::new(opts)
     }
 
     #[derive(Default)]
@@ -205,7 +201,7 @@ mod hotload {
         unsafe fn stub_fn(f: *const ()) {
             use closure_ffi::jit_alloc::{JitAlloc, ProtectJitAccess};
 
-            let stub_template: &[u8] = &__EXTRAS_STUB_TEMPLATE;
+            let stub_template: &[u8] = stub_template_bytes();
 
             let f = f as *const u8;
             let f_w = JIT.lock()
@@ -275,11 +271,28 @@ mod hotload {
         }
     }
 
-    #[cfg(target_arch = "x86_64")]
+    unsafe impl Sync for ExtrasCallbacks {}
+
+    // TODO: macro to construct jmp/call template wrappers then remove closure-ffi
+
+    /// XXX: this all feels silly when hard-coding the encoded bytes
+    /// wouldn't be that unreasonable .-.
+    fn stub_template_bytes() -> &'static [u8] {
+        unsafe {
+            match () {
+                #[cfg(any(target_arch = "x86_64"))]
+                _ => &__EXTRAS_STUB_TEMPLATE,
+                #[cfg(not(any(target_arch = "x86_64")))]
+                _ => &*(__extras_stub_template as unsafe extern "C" fn() as usize as *const [u8; 8]),
+            }
+        }
+    }
+
+    #[cfg(any(target_arch = "x86_64"))]
     extern "C" {
+        #[link_name = "__extras_stub_template"]
         static __EXTRAS_STUB_TEMPLATE: [u8; 8];
     }
-    // TODO: this is silly just hard-code it
     #[cfg(target_arch = "x86_64")]
     core::arch::global_asm! {
         ".global {stub_template_return}",
@@ -291,5 +304,20 @@ mod hotload {
         stub_template_return = sym __EXTRAS_STUB_TEMPLATE,
     }
 
-    unsafe impl Sync for ExtrasCallbacks {}
+    /* XXX: once rustc is updated to 1.88.0, naked functions may be a viable alternative:
+    #[cfg(any(target_arch = "x86_64"))]
+    #[unsafe(naked)]
+    #[link_section = ".data"] // I wonder...
+    //#[no_mangle]
+    unsafe extern "C" fn __extras_stub_template() {
+        core::arch::naked_asm!(
+            "ret"
+            "nop"
+        );
+    }*/
+
+    #[cfg(not(any(target_arch = "x86_64")))]
+    #[inline(never)]
+    #[deprecated = "naive fallback, build architecture not supported"]
+    unsafe extern "C" fn __extras_stub_template() {}
 }
