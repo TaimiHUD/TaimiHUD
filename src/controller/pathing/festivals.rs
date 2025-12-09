0805500886a31f19130fdf09f9dd976bf3f862bf
arcnmx <git@git.arcn.mx>
2025-12-08 18:59:52 -0800

    feat(controller): ApiController owls and whatnot
    what's a flock of owls called anyway, can one disturb a murder of owls?

--- src/controller/pathing/festivals.rs
+++ src/controller/pathing/festivals.rs
@@ -118,39 +118,6 @@ impl FestivalFixup {
         "reactif.festivals",
         "legs.festivals",
     ];
-
-    /// [self.active_festivals](SystemTime::now)
-    pub fn current_festivals() -> Festivals {
-        Self::active_festivals(SystemTime::now())
-    }
-    /// For use as an initial setting, not to be considered reliable
-    pub fn active_festivals(now: SystemTime) -> Festivals {
-        Self::FESTIVAL_WINDOWS
-            .iter()
-            .filter(move |(_f, window)| window.is_active(now))
-            .map(|&(festival, ..)| Festivals::from(festival))
-            .collect()
-    }
-
-    /// Generally Tuesdays from $(date +%s -d '20??-??-??T09:00:00-07:00')
-    /// to $(date +%s -d '20??-??-??T12:00:00-07:00')
-    pub const FESTIVAL_WINDOWS: &'static [(Festival, FestivalWindow)] = &[
-        // Shadow of the Mad King 2025: 2025-10-07 — 2025-11-04
-        (
-            Festival::Halloween,
-            FestivalWindow::with_timestamp(1759852800, 1762282800),
-        ),
-        // Wintersday 2025: 2025-12-09 — 2026-01-06
-        (
-            Festival::Wintersday,
-            FestivalWindow::with_timestamp(1765296000, 1767726000),
-        ),
-        // Lunar New Year 2026: 2026-02-03 — 2026-02-24
-        (
-            Festival::LunarNewYear,
-            FestivalWindow::with_timestamp(1770134400, 1771959600),
-        ),
-    ];
 }
 
 #[derive(Debug, Clone, PartialOrd, Ord, PartialEq, Eq, Hash)]
@@ -207,4 +174,37 @@ impl FestivalWindow {
             _ => false,
         }
     }
+
+    /// [self.active_festivals](SystemTime::now)
+    pub fn current_festivals() -> Festivals {
+        Self::active_festivals(SystemTime::now())
+    }
+    /// For use as an initial setting, not to be considered reliable
+    pub fn active_festivals(now: SystemTime) -> Festivals {
+        Self::FESTIVAL_WINDOWS
+            .iter()
+            .filter(move |(_f, window)| window.is_active(now))
+            .map(|&(festival, ..)| Festivals::from(festival))
+            .collect()
+    }
+
+    /// Generally Tuesdays from $(date +%s -d '20??-??-??T09:00:00-07:00')
+    /// to $(date +%s -d '20??-??-??T12:00:00-07:00')
+    pub const FESTIVAL_WINDOWS: &'static [(Festival, FestivalWindow)] = &[
+        // Shadow of the Mad King 2025: 2025-10-07 — 2025-11-04
+        (
+            Festival::Halloween,
+            FestivalWindow::with_timestamp(1759852800, 1762282800),
+        ),
+        // Wintersday 2025: 2025-12-09 — 2026-01-02
+        (
+            Festival::Wintersday,
+            FestivalWindow::with_timestamp(1765296000, 1767376800),
+        ),
+        // Lunar New Year 2026: 2026-02-03 — 2026-02-24
+        (
+            Festival::LunarNewYear,
+            FestivalWindow::with_timestamp(1770134400, 1771959600),
+        ),
+    ];
 }
