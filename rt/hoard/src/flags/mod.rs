e86f77fd8ba8479f0c5c4aa266882b02cf950f79
arcnmx <git@git.arcn.mx>
2025-12-17 04:10:53 -0800

    chore(rt): collections

--- rt/hoard/src/flags/mod.rs
+++ rt/hoard/src/flags/mod.rs
@@ -1,5 +1,27 @@
-pub use self::serde::{BitFlagContainer, BitFlagDisplay};
+pub use {
+    self::{
+        serde::{BitFlagContainer, BitFlagDisplay},
+        set::{BitSet, FlagSet},
+    },
+    bitvec::{
+        self,
+        array::{self as bitarray, BitArray},
+        boxed::{self as bitbox, BitBox},
+        index as bitidx,
+        order::{self as bitorder, BitOrder, LocalBits as BitsNative, Lsb0 as BitsLsb, Msb0 as BitsMsb},
+        ptr::{self as bitptr, BitPtr, BitRef, Mutability as BitMutability},
+        slice::{self as bitslice, BitSlice},
+        store::{self as bitstore, BitStore},
+        vec::BitVec,
+        view::{self as bitview, BitView},
+    },
+    wyz::comu::{self, Address as BitAddr},
+};
+
 #[cfg(feature = "serde")]
 pub use self::serde::{BitFlagDe, BitFlagSer};
 
 mod serde;
+pub mod set;
+#[cfg(todo)]
+pub mod shared;
