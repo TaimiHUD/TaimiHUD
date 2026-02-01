03931978ba9636f95d91b9011a453520c1d7e0f5
arcnmx <git@git.arcn.mx>
2026-02-01 07:52:21 -0800

    chore(rt): flatset

--- rt/hoard/src/collections/mod.rs
+++ rt/hoard/src/collections/mod.rs
@@ -1,5 +1,9 @@
-pub use self::traits::{TaimiExtend, TaimiSet};
+pub use self::{
+    flatset::FlatSet,
+    traits::{TaimiExtend, TaimiSet},
+};
 
+pub mod flatset;
 pub mod traits;
 
 /// TODO: check if points to same slice .-.
