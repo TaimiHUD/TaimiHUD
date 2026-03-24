f975bb26cd1b92bf5cd3bf70d3f84b821ccf3828
arcnmx <git@git.arcn.mx>
2026-03-24 12:39:08 -0700

    chore(pack): rearrange caches
    also fix incorrect decoder sanity check

--- pathing/meta/build.rs
+++ pathing/meta/build.rs
@@ -7,7 +7,7 @@ use {
         fmt,
         fs,
         io::{self, BufRead, Write},
-        path::PathBuf,
+        path::{Path, PathBuf},
     },
 };
 
@@ -20,35 +20,64 @@ fn main() {
     println!("cargo::rerun-if-env-changed={FEATURE_GZIP}");
     println!("cargo::rerun-if-changed=build.rs");
 
-    match write_map_cache() {
+    write_map_cache();
+}
+
+fn write_map_cache() {
+    if has_gzip() {
+        println!("cargo::rerun-if-env-changed={OUT_DIR}");
+        println!("cargo::rerun-if-env-changed={MANIFEST_DIR}");
+    }
+    process_cache_data(&INC_MAP_CACHE_GZ, map_cache, &INC_MAP_CACHE, Some(MAP_SIGN_FILENAME.as_ref()));
+}
+
+const INC_MAP_CACHE: &'static str = "INC_MAP_CACHE";
+const INC_MAP_CACHE_GZ: &'static str = "INC_MAP_CACHE_GZ";
+const MAP_FILENAME: &'static str = "maps.json";
+const MAP_SIGN_FILENAME: &'static str = "maps-sign.json";
+fn map_cache(mut parent: PathBuf) -> PathBuf {
+    parent.push(MAP_FILENAME);
+    parent
+}
+
+fn process_cache_data<F>(id: &dyn fmt::Display, mut filename: F, src_id: &dyn fmt::Display, fallback: Option<&Path>) where
+    F: FnMut(PathBuf) -> PathBuf,
+{
+    match write_cache_data(src_id, &mut filename, fallback) {
         Ok(Some(map_path)) => {
-            println!("cargo::rustc-env={INC_MAP_CACHE_GZ}={}", map_path.display());
+            println!("cargo::rustc-env={id}={}", map_path.display());
         },
         Ok(None) => (),
         Err(e) => {
-            println!("cargo::warning=failed to write cache file: {e:?}");
-            if let Ok(dest_path) = out_dir().map(map_cache) {
+            println!("cargo::warning=failed to write {id} cache: {e}");
+            if let Ok(dest_path) = out_dir().map(filename) {
                 let _ = fs::remove_file(dest_path);
             }
         },
     }
 }
+fn write_cache_data<F>(id: &dyn fmt::Display, mut filename: F, fallback: Option<&Path>) -> Result<Option<PathBuf>, Error> where
+    F: FnMut(PathBuf) -> PathBuf,
+{
+    let src_dir = data_dir()?;
 
-fn write_map_cache() -> Result<Option<PathBuf>, Error> {
-    if !env::var_os(FEATURE_GZIP).is_some() {
-        if let Ok(mut sign) = data_dir() {
-            sign.push(MAP_SIGN_FILENAME);
-            println!("cargo::rustc-env={INC_MAP_CACHE}={}", sign.display());
-        }
+    if !has_gzip() {
+        let src = match fallback {
+            Some(fallback) => {
+                let mut src = src_dir;
+                src.push(fallback);
+                src
+            },
+            None => filename(src_dir),
+        };
+        println!("cargo::rustc-env={id}={}", src.display());
         return Ok(None)
     }
-    println!("cargo::rerun-if-env-changed={OUT_DIR}");
-    println!("cargo::rerun-if-env-changed={MANIFEST_DIR}");
 
-    let src = data_dir().map(map_cache)?;
+    let src = filename(src_dir);
     println!("cargo::rerun-if-changed={}", src.display());
-    println!("cargo::rustc-env={INC_MAP_CACHE}={}", src.display());
-    let dest_path = out_dir().map(map_cache)?;
+    println!("cargo::rustc-env={id}={}", src.display());
+    let dest_path = out_dir().map(filename)?;
 
     let src = fs::File::open(src)?;
     let mut encoder = GzipEncoder::new(Level::Fastest.into());
@@ -61,6 +90,7 @@ fn write_map_cache() -> Result<Option<PathBuf>, Error> {
     let mut src = io::BufReader::with_capacity(0x1000, src);
 
     let mut out = PartialBuffer::new(vec![0u8; 0x800]);
+    let mut src_len = 0usize;
     loop {
         let mut buf = PartialBuffer::new(match src.buffer() {
             b if b.is_empty() => src.fill_buf()?,
@@ -72,6 +102,7 @@ fn write_map_cache() -> Result<Option<PathBuf>, Error> {
         encoder.encode(&mut buf, &mut out)?;
         let consumed = buf.written().len();
         src.consume(consumed);
+        src_len += consumed;
         if out.written().len() > out.unwritten().len() || true {
             dest.write_all(out.written())?;
             out.reset();
@@ -84,6 +115,7 @@ fn write_map_cache() -> Result<Option<PathBuf>, Error> {
     }
     dest.write_all(out.written())?;
     out.reset();
+    println!("cargo::rustc-env={id}_BUFLEN={}", src_len.next_multiple_of(0x200));
 
     dest.flush()?;
     dest.sync_data()?;
@@ -92,8 +124,6 @@ fn write_map_cache() -> Result<Option<PathBuf>, Error> {
 }
 
 const FEATURE_GZIP: &'static str = "CARGO_FEATURE_GZIP";
-const INC_MAP_CACHE: &'static str = "INC_MAP_CACHE";
-const INC_MAP_CACHE_GZ: &'static str = "INC_MAP_CACHE_GZ";
 const OUT_DIR: &'static str = "OUT_DIR";
 const MANIFEST_DIR: &'static str = "CARGO_MANIFEST_DIR";
 fn out_dir() -> Result<PathBuf, Error> {
@@ -111,10 +141,6 @@ fn data_dir() -> Result<PathBuf, Error> {
     root.push("data");
     Ok(root)
 }
-const MAP_FILENAME: &'static str = "maps.json";
-const MAP_SIGN_FILENAME: &'static str = "maps-sign.json";
-fn map_cache(parent: impl Into<PathBuf>) -> PathBuf {
-    let mut parent = parent.into();
-    parent.push(MAP_FILENAME);
-    parent
+fn has_gzip() -> bool {
+    env::var_os(FEATURE_GZIP).is_some()
 }
