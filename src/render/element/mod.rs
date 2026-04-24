c22800df4fd083ef6b53a32da30caef67ce71e3c
arcnmx <git@git.arcn.mx>
2026-04-23 19:28:39 -0700

    element::prelude

--- src/render/element/mod.rs
+++ src/render/element/mod.rs
@@ -6,3 +6,10 @@ pub mod language;
 #[cfg(feature = "paths")]
 pub mod pack;
 pub mod token;
+
+pub mod prelude {
+    pub use {
+        crate::with_i18n,
+        super::im::{self, prelude::*},
+    };
+}
