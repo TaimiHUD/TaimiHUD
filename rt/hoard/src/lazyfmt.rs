dc8e9b4e8ba963ffdbaa22ca91ad0a87051aaf67
arcnmx <git@git.arcn.mx>
2026-05-13 06:10:18 -0700

    chore(rt): update lazyfmt too

--- rt/hoard/src/lazyfmt.rs
+++ rt/hoard/src/lazyfmt.rs
@@ -3,16 +3,20 @@ use {
     core::{
         cell::{Cell, OnceCell, RefCell},
         fmt,
+        mem,
         ops,
     },
 };
 
 #[derive(Copy, Clone)]
-pub struct MaybeFmt<F = FormatterFn>(pub F);
+#[repr(transparent)]
+pub struct MaybeFmt<F: ?Sized = FormatterFn>(pub F);
 impl<F> MaybeFmt<F> {
+    #[inline(always)]
     pub const fn with(f: F) -> Self {
         Self(f)
     }
+    #[inline(always)]
     pub const fn new(f: F) -> Self
     where
         F: Fn(&mut fmt::Formatter) -> fmt::Result,
@@ -34,7 +38,96 @@ impl<F> MaybeFmt<F> {
         MaybeFmt::new(Self::wrap_lazy_fn::<R, F>(f))
     }
 }
-impl<F> fmt::Display for MaybeFmt<F>
+impl<F: ?Sized> MaybeFmt<F> {
+    #[inline(always)]
+    pub const fn from_ref(f: &F) -> &Self {
+        unsafe { mem::transmute(f) }
+    }
+    #[inline(always)]
+    pub fn from_mut(f: &mut F) -> &mut Self {
+        unsafe { mem::transmute(f) }
+    }
+    #[inline(always)]
+    pub const fn to_ref(&self) -> &MaybeFmt<F> {
+        MaybeFmt::from_ref(&self.0)
+    }
+    #[inline(always)]
+    pub fn to_mut(&mut self) -> &mut MaybeFmt<F> {
+        MaybeFmt::from_mut(&mut self.0)
+    }
+    pub const fn as_dyn<'a>(&'a self) -> &'a MaybeFmt<dyn Fn(&mut fmt::Formatter) -> fmt::Result + 'a>
+    where
+        F: Fn(&mut fmt::Formatter) -> fmt::Result + Sized,
+    {
+        MaybeFmt::from_ref(&self.0 as &dyn Fn(&mut fmt::Formatter) -> fmt::Result)
+    }
+    #[cfg(todo)]
+    pub const fn to_dyn<'a>(&'a self) -> MaybeFmt<&'a dyn Fn(&mut fmt::Formatter) -> fmt::Result>
+    where
+        F: Fn(&mut fmt::Formatter) -> fmt::Result + Sized,
+    {
+        MaybeFmt::from_ref(&self.0 as &dyn Fn(&mut fmt::Formatter) -> fmt::Result)
+    }
+}
+impl MaybeFmt<FormatterFn> {
+    /// format `Some(T)` or fallback
+    pub const fn fmt_or<T: fmt::Display, F: fmt::Display>(
+        v: Option<T>,
+        fallback: F,
+    ) -> MaybeFmt<impl Fn(&mut fmt::Formatter) -> fmt::Result> {
+        MaybeFmt::new(move |f| match &v {
+            Some(v) => fmt::Display::fmt(v, f),
+            None => fmt::Display::fmt(&fallback, f),
+        })
+    }
+    /// format `Some(T)` or fallback
+    pub const fn fmt_ok_or<T: fmt::Display, U: fmt::Display>(
+        v: Result<T, U>,
+    ) -> MaybeFmt<impl Fn(&mut fmt::Formatter) -> fmt::Result> {
+        MaybeFmt::new(move |f| match &v {
+            Ok(v) => fmt::Display::fmt(v, f),
+            Err(v) => fmt::Display::fmt(v, f),
+        })
+    }
+}
+#[inline(always)]
+pub const fn fmt_fn<F: Fn(&mut fmt::Formatter) -> fmt::Result>(f: F) -> MaybeFmt<F> {
+    MaybeFmt::with(f)
+}
+#[inline(always)]
+pub const fn fmt_once<F: FnOnce(&mut fmt::Formatter) -> fmt::Result>(f: F) -> MaybeFmtOnce<F> {
+    MaybeFmtOnce::with(f)
+}
+#[inline(always)]
+pub const fn fmt_mut<F: FnMut(&mut fmt::Formatter) -> fmt::Result>(f: F) -> MaybeFmtMut<F> {
+    MaybeFmtMut::with(f)
+}
+#[inline(always)]
+pub const fn fmt_or<T: fmt::Display, F: fmt::Display>(
+    v: Option<T>,
+    fallback: F,
+) -> MaybeFmt<impl Fn(&mut fmt::Formatter) -> fmt::Result> {
+    MaybeFmt::fmt_or(v, fallback)
+}
+#[inline(always)]
+pub const fn or_empty<T: fmt::Display>(
+    v: Option<T>,
+) -> MaybeFmt<impl Fn(&mut fmt::Formatter) -> fmt::Result> {
+    fmt_or(v, "")
+}
+#[inline(always)]
+pub const fn or_unavail<T: fmt::Display>(
+    v: Option<T>,
+) -> MaybeFmt<impl Fn(&mut fmt::Formatter) -> fmt::Result> {
+    fmt_or(v, UNAVAILABLE)
+}
+#[inline(always)]
+pub const fn ok_or<T: fmt::Display, U: fmt::Display>(
+    v: Result<T, U>,
+) -> MaybeFmt<impl Fn(&mut fmt::Formatter) -> fmt::Result> {
+    MaybeFmt::fmt_ok_or(v)
+}
+impl<F: ?Sized> fmt::Display for MaybeFmt<F>
 where
     F: Fn(&mut fmt::Formatter) -> fmt::Result,
 {
@@ -42,7 +135,7 @@ where
         (self.0)(f)
     }
 }
-impl<F> fmt::Debug for MaybeFmt<F>
+impl<F: ?Sized> fmt::Debug for MaybeFmt<F>
 where
     F: Fn(&mut fmt::Formatter) -> fmt::Result,
 {
@@ -50,7 +143,10 @@ where
         fmt::Display::fmt(self, f)
     }
 }
-impl<F> From<F> for MaybeFmt<F> {
+impl<F> From<F> for MaybeFmt<F>
+where
+    F: Fn(&mut fmt::Formatter) -> fmt::Result,
+{
     fn from(f: F) -> Self {
         Self(f)
     }
@@ -60,6 +156,14 @@ where
     F: Fn(&mut fmt::Formatter) -> fmt::Result,
 {
     fn from(f: MaybeFmt<F>) -> Self {
+        f.to_string()
+    }
+}
+impl<F: ?Sized> From<&'_ MaybeFmt<F>> for String
+where
+    F: Fn(&mut fmt::Formatter) -> fmt::Result,
+{
+    fn from(f: &MaybeFmt<F>) -> Self {
         f.to_string()
     }
 }
@@ -139,10 +243,12 @@ where
         f.to_string()
     }
 }
+#[repr(transparent)]
 pub struct MaybeFmtOnce<F = FormatterFn>(pub Cell<Option<F>>);
 impl<F> MaybeFmtOnce<F> {
     pub const EMPTY: Self = Self(Cell::new(None));
 
+    #[inline(always)]
     pub const fn with(f: F) -> Self {
         Self(Cell::new(Some(f)))
     }
@@ -344,3 +450,126 @@ where
         f.get_str()
     }
 }
+
+#[macro_export]
+macro_rules! fmt_args {
+    (dyn $($tt:tt)*) => {
+        $crate::lazyfmt::fmt_args!(
+            $($tt)*
+        ).as_dyn()
+    };
+    (move $($tt:tt)*) => {
+        $crate::lazyfmt::lazyfmt! {
+            @closureargs(move)(; $($tt)*);
+        }
+    };
+    (* dyn $fmt:literal$(, $($tt:tt)*)?) => {
+        $crate::lazyfmt::fmt_args! {
+            dyn * $fmt$(, $($tt)*)?
+        }
+    };
+    (&dyn mut $fmt:literal$(, $($tt:tt)*)?) => {
+        $crate::lazyfmt::fmt_args! {
+            dyn &mut $fmt$(, $($tt)*)?
+        }
+    };
+    (&dyn ref $fmt:literal$(, $($tt:tt)*)?) => {
+        $crate::lazyfmt::fmt_args! {
+            dyn &ref $fmt$(, $($tt)*)?
+        }
+    };
+    (&dyn $fmt:literal$(, $($tt:tt)*)?) => {
+        $crate::lazyfmt::fmt_args! {
+            dyn &ref $fmt$(, $($tt)*)?
+        }
+    };
+    (*& $fmt:literal$(, $($tt:tt)*)?) => {
+        $crate::lazyfmt::lazyfmt! {
+            @closureargs()(; * $fmt$(, $($tt)*)?);
+        }
+    };
+    (* $fmt:literal$(, $($tt:tt)*)?) => {
+        $crate::lazyfmt::lazyfmt! {
+            @closureargs(move)(; * $fmt$(, $($tt)*)?);
+        }
+    };
+    (&mut $fmt:literal$(, $($tt:tt)*)?) => {
+        $crate::lazyfmt::lazyfmt! {
+            @closureargs()(; &mut $fmt$(, $($tt)*)?);
+        }
+    };
+    (&ref $fmt:literal$(, $($tt:tt)*)?) => {
+        $crate::lazyfmt::lazyfmt! {
+            @closureargs()(; &ref $fmt$(, $($tt)*)?);
+        }
+    };
+    (& $fmt:literal$(, $($tt:tt)*)?) => {
+        $crate::lazyfmt::lazyfmt! {
+            @closureargs()(; &ref $fmt$(, $($tt)*)?);
+        }
+    };
+    ($fmt:literal$(, $($tt:tt)*)?) => {
+        $crate::lazyfmt::lazyfmt! {
+            @closureargs(move)(; $fmt$(, $($tt)*)?);
+        }
+    };
+}
+#[macro_export]
+macro_rules! lazyfmt {
+    (move |$f:ident| $expr:expr) => {
+        $crate::lazyfmt::lazyfmt! {
+            @closure(move)(fmt_fn; |f| $expr);
+        }
+    };
+    (* |$f:ident| $expr:expr) => {
+        $crate::lazyfmt::lazyfmt! {
+            @closure()(fmt_once; |f| $expr);
+        }
+    };
+    (&mut |$f:ident| $expr:expr) => {
+        $crate::lazyfmt::lazyfmt! {
+            @closure()(fmt_mut; |f| $expr);
+        }
+    };
+    (&ref |$f:ident| $expr:expr) => {
+        $crate::lazyfmt::lazyfmt! {
+            @closure()(fmt_fn; |f| $expr);
+        }
+    };
+    (& |$f:ident| $expr:expr) => {
+        $crate::lazyfmt::lazyfmt! {
+            @closure()(fmt_fn; |f| $expr);
+        }
+    };
+    (|$f:ident| $expr:expr) => {
+        $crate::lazyfmt::lazyfmt! {
+            @closure()(fmt_fn; |f| $expr);
+        }
+    };
+    (@closure($($preclosure:tt)*)($constructor:ident; |$f:ident| $expr: expr);) => {
+        $crate::lazyfmt::$constructor($($preclosure)* |$f: &mut ::core::fmt::Formatter| -> ::core::fmt::Result {
+            $expr
+        })
+    };
+    (@closureargs $preclosure:tt (; * $($fmt:tt)+);) => {
+        $crate::lazyfmt::lazyfmt! {
+            @closure $preclosure (fmt_once; |f| f.write_fmt(format_args!($($fmt)*)));
+        }
+    };
+    (@closureargs $preclosure:tt (; &mut $($fmt:tt)+);) => {
+        $crate::lazyfmt::lazyfmt! {
+            @closure $preclosure (fmt_mut; |f| f.write_fmt(format_args!($($fmt)*)));
+        }
+    };
+    (@closureargs $preclosure:tt (; &ref $($fmt:tt)+);) => {
+        $crate::lazyfmt::lazyfmt! {
+            @closure $preclosure (fmt_fn; |f| f.write_fmt(format_args!($($fmt)*)));
+        }
+    };
+    (@closureargs $preclosure:tt (; $fmt:literal $($fmt_arg:tt)*);) => {
+        $crate::lazyfmt::lazyfmt! {
+            @closure $preclosure (fmt_fn; |f| f.write_fmt(format_args!($fmt $($fmt_arg)*)));
+        }
+    };
+}
+pub use {fmt_args, lazyfmt};
