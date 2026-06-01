5fd33b1876c63a20fc09957307eb462f649d5a99
arcnmx <git@git.arcn.mx>
2026-05-31 22:09:03 -0700

    chore(ffi): UnsaferCell

--- rt/ffi/src/lib.rs
+++ rt/ffi/src/lib.rs
@@ -1,6 +1,8 @@
 //! most of this hopefully is just a temporary staging ground for things
 //! before they're ready to be pulled into [arcffi]...
 
+use core::{cell::UnsafeCell, ops};
+
 pub mod cstr;
 pub mod data;
 #[cfg(feature = "fnalloc")]
@@ -24,3 +26,61 @@ pub use ::arcffi::{
     NonNull, c_void, c_int, c_uint, c_long, c_ulong, c_longlong, c_ulonglong, c_char, c_uchar, c_schar,
     c_wchar, c_senum, c_uenum, c_bool, c_bool32,
 };
+
+#[repr(transparent)]
+#[derive(Debug, Default)]
+pub struct UnsaferCell<T: ?Sized>(pub UnsafeCell<T>);
+unsafe impl<T: ?Sized> Sync for UnsaferCell<T> {}
+unsafe impl<T: ?Sized> Send for UnsaferCell<T> {}
+impl<T: ?Sized> UnsaferCell<T> {
+    #[inline(always)]
+    pub const unsafe fn new(v: T) -> Self
+    where
+        T: Sized,
+    {
+        Self(UnsafeCell::new(v))
+    }
+    #[inline(always)]
+    pub const unsafe fn from_ref(v: &UnsafeCell<T>) -> &Self {
+        &*(v as *const UnsafeCell<T> as *const Self)
+    }
+    #[inline(always)]
+    pub const unsafe fn from_mut(v: &mut UnsafeCell<T>) -> &mut Self {
+        &mut *(v as *mut UnsafeCell<T> as *mut Self)
+    }
+    #[inline(always)]
+    pub const unsafe fn from_ptr<'a>(v: *mut T) -> &'a Self {
+        &*(v as *mut UnsafeCell<T> as *const _ as *const Self)
+    }
+
+    #[inline(always)]
+    pub const fn get(&self) -> *mut T {
+        self.0.get()
+    }
+    #[inline(always)]
+    pub const fn raw_get(this: *const Self) -> *mut T {
+        UnsafeCell::raw_get(this as *const UnsafeCell<T>)
+    }
+
+    #[inline(always)]
+    pub const unsafe fn as_ref_unchecked(&self) -> &T {
+        &*self.0.get()
+    }
+    #[inline(always)]
+    pub unsafe fn as_mut_unchecked(&self) -> &mut T {
+        &mut *self.0.get()
+    }
+}
+impl<T: ?Sized> ops::Deref for UnsaferCell<T> {
+    type Target = UnsafeCell<T>;
+    #[inline(always)]
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl<T: ?Sized> ops::DerefMut for UnsaferCell<T> {
+    #[inline(always)]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
